2a86c88465a100250519978f2d9225146ec0c864
_pmat_ <matteo@mageia.it>
2013-02-07 22:09:47 +0100

    fixed license header

--- mainwindow.h
+++ mainwindow.h
@@ -1,14 +1,14 @@
 /**
     Copyright (C) 2012-2013  Matteo Pasotti <matteo.pasotti@gmail.com>
 
-    This file is part of fgam - FlightGear Aircraft Manager
+    This file is part of yainstall - Yet Another flightgear Aircraft Installer
 
-    fgam is free software: you can redistribute it and/or modify
+    yainstall is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    fgam is distributed in the hope that it will be useful,
+    yainstall is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
